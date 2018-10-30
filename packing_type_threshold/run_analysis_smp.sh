42c3fd119933cc744f375bb67336973dbe4289e4
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2018-10-30 17:32:19 +0900

    added cholesky taskoing benchmarks from RIKEN and BSC to work on implementation for that

--- packing_type_threshold/run_analysis_smp.sh
+++ packing_type_threshold/run_analysis_smp.sh
@@ -3,18 +3,23 @@
 #BSUB -o batch_analyse_threshold_packing_type.out.%J
 #BSUB -e batch_analyse_threshold_packing_type.err.%J
 #BSUB -W 09:00
-#BSUB -m c24m128
+##BSUB -m c24m128
+#BSUB -m c144m1024
 #BSUB -n 1
 #BSUB -x
 #BSUB -a openmp
 #BSUB -P jara0001
-#BSUB -M 126000
+##BSUB -M 126000
+#BSUB -M 1000000
 #BSUB -u j.klinkenberg@itc.rwth-aachen.de
 #BSUB -B
 #BSUB -N
 
+
 module use /home/jk869269/.modules
 source /home/jk869269/util/includes/env_chameleon_dev.sh
+
+export I_MPI_FABRICS="shm:dapl"
 
 module unload chameleon-lib
 module load chameleon-lib/1.0
