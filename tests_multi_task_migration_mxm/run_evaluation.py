5452f76ffd028e75625467c240d762685bc2ff9e
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2019-06-12 11:11:28 +0200

    implemented result plotting for multi migration tests

--- tests_multi_task_migration_mxm/run_evaluation.py
+++ tests_multi_task_migration_mxm/run_evaluation.py
@@ -4,9 +4,95 @@ import statistics as st
 from datetime import datetime
 import csv
 from CResult import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotData(target_file_path, arr_x_axis, arr_types, arr_time_baseline, arr_time_chameleon, arr_remote_tasks, text_header, text_x_axis):
+    xtick = list(range(len(arr_x_axis)))
+
+    # ========== Time Plot
+    path_result_img = target_file_path + "_time.png"
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.gca()
+    # baseline first
+    labels = ['OpenMP baseline']
+    ax.plot(xtick, np.array(arr_time_baseline[0]), 'x-', linewidth=2)
+    # now versions
+    for idx_type in range(len(arr_types)):
+        ax.plot(xtick, np.array(arr_time_chameleon[idx_type]), 'x-', linewidth=2)
+        labels.append(arr_types[idx_type])
+    plt.xticks(xtick, arr_x_axis)
+    ax.minorticks_on()
+    ax.legend(labels, fancybox=True, shadow=False)                
+    ax.grid(b=True, which='major', axis="both", linestyle='-', linewidth=1)
+    ax.grid(b=True, which='minor', axis="both", linestyle='-', linewidth=0.4)
+    ax.set_xlabel(text_x_axis)
+    ax.set_ylabel("Time [sec]")
+    ax.set_title(text_header + " - Execution Time" )
+    fig.savefig(path_result_img, dpi=None, facecolor='w', edgecolor='w', 
+        format="png", transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    plt.close(fig)
+
+    # ========== Speedup Plot
+    path_result_img = target_file_path + "_speedup.png"
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.gca()
+    # baseline first
+    labels = ['OpenMP baseline']
+    ax.plot(xtick, np.array([1 for x in xtick]), 'x-', linewidth=2)
+    # now versions
+    for idx_type in range(len(arr_types)):
+        tmp_speedups = [arr_time_baseline[0][x] / arr_time_chameleon[idx_type][x] for x in range(len(arr_x_axis))]
+        ax.plot(xtick, np.array(tmp_speedups), 'x-', linewidth=2)
+        labels.append(arr_types[idx_type])
+    plt.xticks(xtick, arr_x_axis)
+    ax.minorticks_on()
+    ax.legend(labels, fancybox=True, shadow=False)
+    ax.grid(b=True, which='major', axis="both", linestyle='-', linewidth=1)
+    ax.grid(b=True, which='minor', axis="both", linestyle='-', linewidth=0.4)
+    ax.set_xlabel(text_x_axis)
+    ax.set_ylabel("Speedup")
+    ax.set_title(text_header + " - Speedup" )
+    fig.savefig(path_result_img, dpi=None, facecolor='w', edgecolor='w', 
+        format="png", transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    plt.close(fig)
+
+    # ========== Number of migrated tasks
+    path_result_img = target_file_path + "_migrated.png"
+    fig = plt.figure(figsize=(16, 9))
+    ax = fig.gca()
+    labels = ['OpenMP baseline']
+    ax.plot(xtick, np.array([0 for x in xtick]), 'x-', linewidth=2)
+    # now versions
+    for idx_type in range(len(arr_types)):
+        ax.plot(xtick, np.array(arr_remote_tasks[idx_type]), 'x-', linewidth=2)
+        labels.append(arr_types[idx_type])
+    plt.xticks(xtick, arr_x_axis)
+    ax.minorticks_on()
+    ax.legend(labels, fancybox=True, shadow=False)
+    ax.grid(b=True, which='major', axis="both", linestyle='-', linewidth=1)
+    ax.grid(b=True, which='minor', axis="both", linestyle='-', linewidth=0.4)
+    ax.set_xlabel(text_x_axis)
+    ax.set_ylabel("# migrated tasks")
+    ax.set_title(text_header + " - Migrated Tasks" )
+    fig.savefig(path_result_img, dpi=None, facecolor='w', edgecolor='w', 
+        format="png", transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    plt.close(fig)
 
 if __name__ == "__main__":
-    source_folder = os.path.dirname(os.path.abspath(__file__))
+    # source_folder = os.path.dirname(os.path.abspath(__file__))
+    source_folder       = "C:\\J.Klinkenberg.Local\\repos\\chameleon\\chameleon-scripts\\tests_multi_task_migration_mxm\\results_8nodes_dm"
+    target_folder_data  = os.path.join(source_folder, "result_data")
+    target_folder_plot  = os.path.join(source_folder, "result_plots")
+
+    # create target directories of not yet existing
+    if not os.path.exists(target_folder_data):
+        os.makedirs(target_folder_data)
+    if not os.path.exists(target_folder_plot):
+        os.makedirs(target_folder_plot)
 
     # list of result containers
     list_results = []
@@ -63,8 +149,10 @@ if __name__ == "__main__":
                 arr_n_tasks_local.append(tmp_n_tasks_local)
                 arr_n_tasks_remote.append(tmp_n_tasks_remote)
 
-        # output/plot results
-        with open("output_granularity_" + str(gran) + ".result", mode='w', newline='') as f:
+        # output results
+        tmp_target_file_name = "output_granularity_" + str(gran) + ".result"
+        tmp_target_file_path = os.path.join(target_folder_data, tmp_target_file_name)
+        with open(tmp_target_file_path, mode='w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
             writer.writerow(['OpenMP'] + unique_n_threads)
             for tmp_idx in range(len(arr_types)):
@@ -86,6 +174,11 @@ if __name__ == "__main__":
             writer.writerow(['N_RemoteTasks'] + unique_n_threads)
             for tmp_idx in range(len(arr_types)):
                 writer.writerow([arr_types[tmp_idx]] + arr_n_tasks_remote[tmp_idx])
+
+        # plot results
+        tmp_target_file_name = "plot_granularity_" + str(int(gran))
+        tmp_target_file_path = os.path.join(target_folder_plot, tmp_target_file_name)
+        plotData(tmp_target_file_path, unique_n_threads, arr_types, arr_time_original, arr_time_chameleon, arr_n_tasks_remote, "Granularity " + str(int(gran)), "# threads per rank")
 
     # ========== Generate results with fixed number of threads
     for thr in unique_n_threads:
@@ -124,8 +217,10 @@ if __name__ == "__main__":
                 arr_n_tasks_local.append(tmp_n_tasks_local)
                 arr_n_tasks_remote.append(tmp_n_tasks_remote)
 
-        # output/plot results
-        with open("output_thread_" + str(thr) + ".result", mode='w', newline='') as f:
+        # output results
+        tmp_target_file_name = "output_thread_" + str(thr) + ".result"
+        tmp_target_file_path = os.path.join(target_folder_data, tmp_target_file_name)
+        with open(tmp_target_file_path, mode='w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
             writer.writerow(['OpenMP'] + unique_granularities)
             for tmp_idx in range(len(arr_types)):
@@ -140,11 +235,15 @@ if __name__ == "__main__":
                 tmp_speedups = [arr_time_original[0][x] / arr_time_chameleon[tmp_idx][x] for x in range(len(unique_granularities))]
                 writer.writerow([arr_types[tmp_idx]] + tmp_speedups)
             writer.writerow([])
-            writer.writerow(['N_LocalTasks'] + unique_n_threads)
+            writer.writerow(['N_LocalTasks'] + unique_granularities)
             for tmp_idx in range(len(arr_types)):
                 writer.writerow([arr_types[tmp_idx]] + arr_n_tasks_local[tmp_idx])
             writer.writerow([])
-            writer.writerow(['N_RemoteTasks'] + unique_n_threads)
+            writer.writerow(['N_RemoteTasks'] + unique_granularities)
             for tmp_idx in range(len(arr_types)):
                 writer.writerow([arr_types[tmp_idx]] + arr_n_tasks_remote[tmp_idx])
 
+        # plot results
+        tmp_target_file_name = "plot_threads_" + str(int(thr))
+        tmp_target_file_path = os.path.join(target_folder_plot, tmp_target_file_name)
+        plotData(tmp_target_file_path, unique_granularities, arr_types, arr_time_original, arr_time_chameleon, arr_n_tasks_remote, str(int(thr)) + " Threads", "# task granularity (matrix size)")
