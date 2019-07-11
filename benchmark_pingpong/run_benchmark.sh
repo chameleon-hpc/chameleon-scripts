ff408e9c025be28f92d6d217c7f6d3891dc7623b
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2019-07-11 13:12:04 +0200

    added script for creating plots for ping pong benchmarks + added results for claix18

--- benchmark_pingpong/run_benchmark.sh
+++ benchmark_pingpong/run_benchmark.sh
@@ -18,18 +18,18 @@ CUR_DATE_STR=${CUR_DATE_STR:-"$(date +"%Y%m%d_%H%M%S")"}
 
 # Tests for Type 0
 make
-make run-sm-socket &> ${CUR_DATE_STR}_result_type0_socket.log
-make run-sm-cache2 &> ${CUR_DATE_STR}_result_type0_cache2.log
-make run-dm &> ${CUR_DATE_STR}_result_type0_dm.log
+make run-sm-socket &> result_type0_socket.log
+make run-sm-cache2 &> result_type0_cache2.log
+make run-dm &> result_type0_dm.log
 
 # Tests for Type 1
 ADDITIONAL_COMPILE_FLAGS="-DBENCHMARK_TYPE=1" make
-make run-sm-socket &> ${CUR_DATE_STR}_result_type1_socket.log
-make run-sm-cache2 &> ${CUR_DATE_STR}_result_type1_cache2.log
-make run-dm &> ${CUR_DATE_STR}_result_type1_dm.log
+make run-sm-socket &> result_type1_socket.log
+make run-sm-cache2 &> result_type1_cache2.log
+make run-dm &> result_type1_dm.log
 
 # Tests for Type 2
 ADDITIONAL_COMPILE_FLAGS="-DBENCHMARK_TYPE=2" make
-make run-sm-socket &> ${CUR_DATE_STR}_result_type2_socket.log
-make run-sm-cache2 &> ${CUR_DATE_STR}_result_type2_cache2.log
-make run-dm &> ${CUR_DATE_STR}_result_type2_dm.log
+make run-sm-socket &> result_type2_socket.log
+make run-sm-cache2 &> result_type2_cache2.log
+make run-dm &> result_type2_dm.log
