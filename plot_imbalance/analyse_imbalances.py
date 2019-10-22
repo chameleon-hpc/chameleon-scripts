187af32c811d4b8185bf67b1c1883e53b08d721f
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2019-10-22 17:06:16 +0200

    updated imbalance plot script to also output that to csv

--- plot_imbalance/analyse_imbalances.py
+++ plot_imbalance/analyse_imbalances.py
@@ -3,6 +3,7 @@
 import os,sys
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 script_dir              = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,20 +22,39 @@ def computeLoadImbalance(input_file_path, nranks):
      stats.parseContent(tmp_arr, pre_filtered=False)
 
   data = np.array([stats.task_exec_overall.time_sum for stats in rank_stats])
-  #print (np.mean(data,0))  
-  #print (np.amax(data,0))
-  #print (np.argmax(data,0))
+
+  cur_max   = np.amax(data,0)
+  cur_mean  = np.mean(data,0)
+  cur_arg_max = np.argmax(data,0)
+  print (cur_mean)  
+  print (cur_max)
+  print (cur_arg_max)
  
-  load_imbalance = np.divide(np.amax(data,0),np.mean(data,0))-1
+  load_imbalance = np.divide(cur_max,cur_mean)-1
+
+  tmp_target_file_path = "C:\\J.Klinkenberg.Local\\repos\\chameleon\\chameleon-scripts\\tests_samoa\\strong_scaling_tests\\imbalances_no_stealing.log"
+  with open(tmp_target_file_path, mode='w', newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    # writer.writerow(['Max Values'])
+    # writer.writerow(cur_max)
+    # writer.writerow(['Mean Values'])
+    # writer.writerow(cur_mean)
+    # writer.writerow(['Argmax Values'])
+    # writer.writerow(cur_arg_max)
+    writer.writerow(['Load Imbalance'])
+    writer.writerow(load_imbalance)
 
   return load_imbalance
 
 if __name__ == "__main__":
-  input_file = sys.argv[1]
+#   input_file = sys.argv[1]
+#   input_file = "C:\\J.Klinkenberg.Local\\repos\\chameleon\\chameleon-scripts\\tests_samoa\\strong_scaling_tests\\imbalances_23threads\\results_stealing_t23_r1_chameleon_strong.log"
+  input_file = "C:\\J.Klinkenberg.Local\\repos\\chameleon\\chameleon-scripts\\tests_samoa\\strong_scaling_tests\\imbalances_12threads\\results_no_stealing_t12_r1_chameleon_strong.log"
   print (input_file)
-  ranks = int(sys.argv[2])
+#   ranks = int(sys.argv[2])
+  ranks = 32
 
-  load_imbalance = computeLoadImbalance(input_file, nranks)
+  load_imbalance = computeLoadImbalance(input_file, ranks)
 
   plt.plot(range(0,np.size(load_imbalance)), load_imbalance)
   plt.show()
