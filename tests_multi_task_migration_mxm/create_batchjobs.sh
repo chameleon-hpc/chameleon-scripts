c8fbbfc6952c83d03fb166ea8f93dbc913c3ab64
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2019-10-29 20:30:57 +0100

    deactivated parsing of throughput for now

--- tests_multi_task_migration_mxm/create_batchjobs.sh
+++ tests_multi_task_migration_mxm/create_batchjobs.sh
@@ -53,9 +53,9 @@ COMMENT
 export IS_DISTRIBUTED=1
 export IS_SEPARATE=0
 export N_PROCS=2
-#sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm --output=mxm_multi_task_migration_2n_dm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
-export IS_SEPARATE=1
-sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm_sep --output=mxm_multi_task_migration_2n_dm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
+sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm --output=mxm_multi_task_migration_2n_dm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
+#export IS_SEPARATE=1
+#sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm_sep --output=mxm_multi_task_migration_2n_dm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 
 : << COMMENT
 # 4 node job - shared memory
@@ -65,17 +65,17 @@ export N_PROCS=4
 sbatch --nodes=1 --ntasks-per-node=4 --cpus-per-task=6 --job-name=mxm_multi_task_migration_4n_sm --output=mxm_multi_task_migration_4n_sm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 export IS_SEPARATE=1
 sbatch --nodes=1 --ntasks-per-node=4 --cpus-per-task=6 --job-name=mxm_multi_task_migration_4n_sm_sep --output=mxm_multi_task_migration_4n_sm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
-#COMMENT
+COMMENT
 
 # 4 node job - distributed memory
 export IS_DISTRIBUTED=1
 export IS_SEPARATE=0
 export N_PROCS=4
 sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_4n_dm --output=mxm_multi_task_migration_4n_dm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
-export IS_SEPARATE=1
-sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_4n_dm_sep --output=mxm_multi_task_migration_4n_dm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
+#export IS_SEPARATE=1
+#sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_4n_dm_sep --output=mxm_multi_task_migration_4n_dm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 
-#: << COMMENT
+: << COMMENT
 # 8 node job - distributed memory
 export IS_DISTRIBUTED=1
 export IS_SEPARATE=0
