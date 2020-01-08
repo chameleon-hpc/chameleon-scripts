023d8861f21a8a14078edb0018c8eb4a50724a05
Philipp Samfass <samfass@in.tum.de>
2020-01-08 13:46:55 +0100

    added folder for gpi experiments on CoolMUC-2

--- tests_gpi_vs_mpi/create_batchjobs.sh
+++ tests_gpi_vs_mpi/create_batchjobs.sh
@@ -1,8 +1,13 @@
-#!/usr/local_rwth/bin/zsh
+#!/bin/bash
 
 # load modules like Intel compiler, Intel MPI
-source env_ch_intel.sh
-module unload chameleon-lib
+#source env_ch_intel.sh
+#module unload chameleon-lib
+
+module use ~/.modules
+module load mpioffloading
+module use -p /lrz/sys/spack/.tmp/test/gpi2/modules/x86_avx2/linux-sles12-x86_64
+module load gpi-2/1.4.0-intel19-impi 
 
 export CUR_DATE_STR="$(date +"%Y%m%d_%H%M%S")"
 CUR_DIR=$(pwd)
@@ -16,35 +21,35 @@ export DIR_CH_SEPARATE_BUILD=$(pwd)/chameleon_separate_build
 export DIR_CH_SEPARATE_INSTALL=$(pwd)/chameleon_separate_install
 
 # Build regular version (chunk)
-rm -rf ${DIR_CH_REGULAR_BUILD} && mkdir ${DIR_CH_REGULAR_BUILD} && cd ${DIR_CH_REGULAR_BUILD}
-cmake -DCMAKE_INSTALL_PREFIX=${DIR_CH_REGULAR_INSTALL} -DCMAKE_BUILD_TYPE=Release ${DIR_CH}
-make -j8
-make install
-cd ${CUR_DIR}
+#rm -rf ${DIR_CH_REGULAR_BUILD} && mkdir ${DIR_CH_REGULAR_BUILD} && cd ${DIR_CH_REGULAR_BUILD}
+#cmake -DCMAKE_INSTALL_PREFIX=${DIR_CH_REGULAR_INSTALL} -DCMAKE_BUILD_TYPE=Release ${DIR_CH}
+#make -j8
+#make install
+#cd ${CUR_DIR}
 
 # Build version with sepearte offloads
-rm -rf ${DIR_CH_SEPARATE_BUILD} && mkdir ${DIR_CH_SEPARATE_BUILD} && cd ${DIR_CH_SEPARATE_BUILD}
-cmake -DCMAKE_INSTALL_PREFIX=${DIR_CH_SEPARATE_INSTALL} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS='-DOFFLOAD_SEND_TASKS_SEPARATELY=1' -DCMAKE_C_FLAGS='-DOFFLOAD_SEND_TASKS_SEPARATELY=1' ${DIR_CH}
-make -j8
-make install
-cd ${CUR_DIR}
+#rm -rf ${DIR_CH_SEPARATE_BUILD} && mkdir ${DIR_CH_SEPARATE_BUILD} && cd ${DIR_CH_SEPARATE_BUILD}
+#cmake -DCMAKE_INSTALL_PREFIX=${DIR_CH_SEPARATE_INSTALL} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS='-DOFFLOAD_SEND_TASKS_SEPARATELY=1' -DCMAKE_C_FLAGS='-DOFFLOAD_SEND_TASKS_SEPARATELY=1' ${DIR_CH}
+#make -j8
+#make install
+#cd ${CUR_DIR}
 
 # set env vars to use lib
-export LD_LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LD_LIBRARY_PATH}"
-export LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LIBRARY_PATH}"
-export INCLUDE="${DIR_CH_REGULAR_INSTALL}/include:${INCLUDE}"
-export CPATH="${DIR_CH_REGULAR_INSTALL}/include:${CPATH}"
+#export LD_LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LD_LIBRARY_PATH}"
+#export LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LIBRARY_PATH}"
+#export INCLUDE="${DIR_CH_REGULAR_INSTALL}/include:${INCLUDE}"
+#export CPATH="${DIR_CH_REGULAR_INSTALL}/include:${CPATH}"
 
 # build matrix example
-make clean -C ${DIR_MXM_EXAMPLE}
-ITERATIVE_VERSION=0 make -C ${DIR_MXM_EXAMPLE}
+#make clean -C ${DIR_MXM_EXAMPLE}
+#ITERATIVE_VERSION=0 make -C ${DIR_MXM_EXAMPLE}
 
 : << COMMENT
 # 2 node job - shared memory
 export IS_DISTRIBUTED=0
 export IS_SEPARATE=0
 export N_PROCS=2
-sbatch --nodes=1 --ntasks-per-node=2 --cpus-per-task=12 --job-name=mxm_multi_task_migration_2n_sm --output=mxm_multi_task_migration_2n_sm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
+sbatch --nodes=1 --ntasks-per-node=2 --cpus-per-task=28 --job-name=mxm_multi_task_migration_2n_sm --output=mxm_multi_task_migration_2n_sm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 export IS_SEPARATE=1
 sbatch --nodes=1 --ntasks-per-node=2 --cpus-per-task=12 --job-name=mxm_multi_task_migration_2n_sm_sep --output=mxm_multi_task_migration_2n_sm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 COMMENT
@@ -53,7 +58,7 @@ COMMENT
 export IS_DISTRIBUTED=1
 export IS_SEPARATE=0
 export N_PROCS=2
-sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm --output=mxm_multi_task_migration_2n_dm.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
+sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=28 --job-name=mxm_multi_task_migration_2n_dm --output=mxm_multi_task_migration_2n_dm.%J.txt --export=IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 #export IS_SEPARATE=1
 #sbatch --nodes=2 --ntasks-per-node=1 --cpus-per-task=24 --job-name=mxm_multi_task_migration_2n_dm_sep --output=mxm_multi_task_migration_2n_dm_sep.%J.txt --export=DIR_CH_REGULAR_INSTALL,DIR_CH_SEPARATE_INSTALL,IS_DISTRIBUTED,IS_SEPARATE,CUR_DATE_STR,N_PROCS run_experiments.sh
 
---
+++ tests_gpi_vs_mpi/run_experiments.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#SBATCH --time=03:00:00
+#SBATCH --exclusive
+##SBATCH --account=jara0001
+#SBATCH --cluster=mpp2
+#SBATCH --partition=mpp2_batch
+
+# =============== Load desired modules
+#source ~/.zshrc
+#source env_ch_intel.sh
+#module unload chameleon-lib
+
+module use ~/.modules 
+module load mpioffloading
+module load gpi-2/1.4.0-intel19-impi
+
+# # hack because currently vtune is not supported in batch usage
+# module load c_vtune
+# export CMD_VTUNE_PREFIX="amplxe-cl –collect hotspots –r ./${CUR_DATE_STR}_profiling_chameleon_${OMP_NUM_THREADS}t -trace-mpi -- "
+
+# =============== Settings & environment variables
+IS_DISTRIBUTED=${IS_DISTRIBUTED:-1}
+IS_SEPARATE=${IS_SEPARATE:-0}
+N_PROCS=${N_PROCS:-2}
+N_REPETITIONS=${N_REPETITIONS:-10}
+CUR_DATE_STR=${CUR_DATE_STR:-"$(date +"%Y%m%d_%H%M%S")"}
+
+TASK_GRANULARITY=(50 100 150 200 250 300 350 400 450 500 550 600)
+# default number of threads for distributed runs
+N_THREADS=(2 4 6 8 10 12 14 16 18 20 22)
+
+# create result directory
+if [ "${IS_DISTRIBUTED}" = "1" ]; then
+    export SUFFIX_RESULT_DIR="dm"
+else
+    export SUFFIX_RESULT_DIR="sm"
+fi
+
+DIR_RESULT="${CUR_DATE_STR}_results/${N_PROCS}procs_${SUFFIX_RESULT_DIR}"
+DIR_MXM_EXAMPLE=${DIR_MXM_EXAMPLE:-../../chameleon_gpi_apps/applications/matrix_example}
+mkdir -p ${DIR_RESULT}
+
+#if [ "${IS_SEPARATE}" = "1" ]; then
+#  # set env vars to use lib
+#  export LD_LIBRARY_PATH="${DIR_CH_SEPARATE_INSTALL}/lib:${LD_LIBRARY_PATH}"
+#  export LIBRARY_PATH="${DIR_CH_SEPARATE_INSTALL}/lib:${LIBRARY_PATH}"
+#  export INCLUDE="${DIR_CH_SEPARATE_INSTALL}/include:${INCLUDE}"
+#  export CPATH="${DIR_CH_SEPARATE_INSTALL}/include:${CPATH}"
+#else
+## set env vars to use lib
+#  export LD_LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LD_LIBRARY_PATH}"
+#  export LIBRARY_PATH="${DIR_CH_REGULAR_INSTALL}/lib:${LIBRARY_PATH}"
+#  export INCLUDE="${DIR_CH_REGULAR_INSTALL}/include:${INCLUDE}"
+#  export CPATH="${DIR_CH_REGULAR_INSTALL}/include:${CPATH}"
+#fi
+
+case ${N_PROCS} in
+    2)
+        echo "Running with 2 procs"
+        MXM_PARAMS="2400 0"
+        if [ "${IS_DISTRIBUTED}" = "0" ]; then
+            N_THREADS=(1 2 3 4 5 6 7 8 9 10 11)
+        fi
+        ;;
+    4)
+        echo "Running with 4 procs"
+        MXM_PARAMS="2400 1600 800 0"
+        if [ "${IS_DISTRIBUTED}" = "0" ]; then
+            N_THREADS=(1 2 3 4 5)
+        fi
+        ;;
+    8)
+        echo "Running with 8 procs"
+        MXM_PARAMS="2400 2057 1714 1371 1028 685 342 0"
+        if [ "${IS_DISTRIBUTED}" = "0" ]; then
+            echo "ERROR: Sorry script does not support shared memory job with 8 procs. Aborting here"
+            exit 2
+        fi
+        ;;
+    *)
+        echo "ERROR: Sorry script does not support number of threads. Aborting here"
+        exit 2
+    esac
+
+export MIN_REL_LOAD_IMBALANCE_BEFORE_MIGRATION=0.1
+export MAX_TASKS_PER_RANK_TO_MIGRATE_AT_ONCE=15
+export PERCENTAGE_DIFF_TASKS_TO_MIGRATE=0.3
+
+# load flags
+#source ../../chameleon/flags_claix_intel.def
+
+if [ "${IS_DISTRIBUTED}" = "0" ]; then
+    MPI_EXEC_CMD="${RUN_SETTINGS} mpiexec.hydra"
+else
+    MPI_EXEC_CMD="${RUN_SETTINGS} mpiexec"
+fi
+
+# =============== Execution Function
+function run_experiments()
+{
+    exec_version=$1
+    if [ "${IS_DISTRIBUTED}" = "0" ]; then
+        exec_version="${exec_version}_sm"
+    else
+        exec_version="${exec_version}_dm"
+    fi
+
+    for g in "${TASK_GRANULARITY[@]}"
+    do
+        for t in "${N_THREADS[@]}"
+        do
+            echo "Running experiments for ${exec_version} and granularity ${g} and n_threads=${t}"
+            export OMP_NUM_THREADS=$t
+            export MIN_LOCAL_TASKS_IN_QUEUE_BEFORE_MIGRATION=$t
+            for r in seq 1 ${N_REPETITIONS}
+            do
+                module switch chameleon chameleon/chameleon_mpi
+                eval "${MPI_EXEC_CMD} -np ${N_PROCS} ${MPI_EXPORT_VARS} ${CMD_VTUNE_PREFIX} ${DIR_MXM_EXAMPLE}/main $g ${MXM_PARAMS}" &> ${DIR_RESULT}/results_${exec_version}_${g}_${t}t_${r}.log
+                module switch chameleon chameleon/chameleon.rel
+                eval "${MPI_EXEC_CMD} -np ${N_PROCS} ${MPI_EXPORT_VARS} ${CMD_VTUNE_PREFIX} ${DIR_MXM_EXAMPLE}/main_gpi $g ${MXM_PARAMS}" &> ${DIR_RESULT}/results_${exec_version}_${g}_${t}t_${r}_gpi.log
+            done
+        done
+    done
+}
+
+if [ "${IS_SEPARATE}" = "1" ]; then
+    run_experiments "separate"
+else
+#    run_experiments "multi"
+    export MAX_TASKS_PER_RANK_TO_MIGRATE_AT_ONCE=1
+    run_experiments "single"
+fi
