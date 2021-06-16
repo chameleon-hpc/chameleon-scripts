d2afd1be71c1b0d97cb72fe43caec8db5d0833eb
marvin.porsil <marvin.porsil@rwth-aachen.de>
2021-06-16 10:09:53 +0200

    Separate Chameleon and Tasking testing.

--- affinity_tests_mxm/create_batchjobs.sh
+++ affinity_tests_mxm/create_batchjobs.sh
@@ -13,9 +13,15 @@ export AUTOMATIC_NUMA_BALANCING=0
 
 export OMP_NUM_THREADS=47
 
+export PROG="mxm_chameleon"
+
 export MXM_PARAMS="1000 100 100 100 100"
 
-export_vars="CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,CHAM_AFF_TASK_SELECTION_STRAT,CHAM_AFF_PAGE_SELECTION_STRAT,CHAM_AFF_PAGE_WEIGHTING_STRAT,CHAM_AFF_CONSIDER_TYPES,CHAM_AFF_PAGE_SELECTION_N,CHAM_AFF_TASK_SELECTION_N,CHAM_AFF_MAP_MODE,CHAM_AFF_ALWAYS_CHECK_PHYSICAL,NO_NUMA_BALANCING,AUTOMATIC_NUMA_BALANCING"
+#export DIR_MXM_EXAMPLE=${DIR_MXM_EXAMPLE:-../../chameleon-apps/applications/matrix_example}
+
+export CHAMELEON_VERSION="chameleon/intel"
+
+export_vars="CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,CHAM_AFF_TASK_SELECTION_STRAT,CHAM_AFF_PAGE_SELECTION_STRAT,CHAM_AFF_PAGE_WEIGHTING_STRAT,CHAM_AFF_CONSIDER_TYPES,CHAM_AFF_PAGE_SELECTION_N,CHAM_AFF_TASK_SELECTION_N,CHAM_AFF_MAP_MODE,CHAM_AFF_ALWAYS_CHECK_PHYSICAL,NO_NUMA_BALANCING,AUTOMATIC_NUMA_BALANCING,PROG,DIR_MXM_EXAMPLE"
 
 #############
 # Domain Mode
@@ -81,21 +87,21 @@ export_vars="CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,CHAM_AFF_TASK_SELECTIO
 #   Test with varying Task selection strategy and mode  #
 #########################################################
 
-export CHAMELEON_VERSION="chameleon/intel"
-export AUTOMATIC_NUMA_BALANCING=1
-export CHAM_AFF_ALWAYS_CHECK_PHYSICAL=1
-for istrat in 0 1 2 3 4;
-do
-    export CHAM_AFF_TASK_SELECTION_STRAT=${istrat};
-    for imode in 0 1;
-    do
-        export CHAM_AFF_MAP_MODE=${imode};
-        sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=affinity_mxm \
-        --output=mxm_test_taskstrat${istrat}_mapmode${imode}.%J.txt \
-        --export=${export_vars} \
-        run_experiments.sh;
-    done
-done
+# export CHAMELEON_VERSION="chameleon/intel"
+# export AUTOMATIC_NUMA_BALANCING=1
+# export CHAM_AFF_ALWAYS_CHECK_PHYSICAL=1
+# for istrat in 0 1 2 3 4;
+# do
+#     export CHAM_AFF_TASK_SELECTION_STRAT=${istrat};
+#     for imode in 0 1;
+#     do
+#         export CHAM_AFF_MAP_MODE=${imode};
+#         sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=affinity_mxm \
+#         --output=mxm_test_taskstrat${istrat}_mapmode${imode}.%J.txt \
+#         --export=${export_vars} \
+#         run_experiments.sh;
+#     done
+# done
 
 ############################
 # Chameleon without affinity
@@ -108,11 +114,21 @@ done
 #--export=CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS \
 #run_experiments.sh
 
-export CHAMELEON_VERSION="chameleon/intel_no_affinity"
-export AUTOMATIC_NUMA_BALANCING=1
-sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=no_affinity_mxm \
---output=mxm_no_affinity_numabal.%J.txt \
---export=CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,AUTOMATIC_NUMA_BALANCING \
-run_experiments.sh
+# export CHAMELEON_VERSION="chameleon/intel_no_affinity"
+# export AUTOMATIC_NUMA_BALANCING=1
+# sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=no_affinity_mxm \
+# --output=mxm_no_affinity_numabal.%J.txt \
+# --export=CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,AUTOMATIC_NUMA_BALANCING \
+# run_experiments.sh
 
 # teste für ohne chameleon matrix beispiel explizit einzeln um OMP_NUM_THREADS=48 nutzen zu können
+##############################
+# Tasking only, no chameleon #
+##############################
+export PROG=mxm_tasking
+export AUTOMATIC_NUMA_BALANCING=1
+export OMP_NUM_THREADS=48
+sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=no_cham_mxm \
+--output=mxm_no_chameleon_numabal.%J.txt \
+--export=CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,AUTOMATIC_NUMA_BALANCING,PROG,DIR_MXM_EXAMPLE \
+run_experiments.sh
--- affinity_tests_mxm/extractData.py
+++ affinity_tests_mxm/extractData.py
@@ -84,7 +84,27 @@ for read_file in os.listdir(outputs_path):
                         break
             if not found:
                 # Parameter not in the file
-                csv_file.write("-1,")
+                if find_string[string_idx][1]=="Time":
+                    # non chameleon version, find other time
+                    other_time_string = "Computations with normal tasking took"
+                    for line in lines:
+                        line = line.rstrip()
+                        param_found = re.findall(other_time_string, line)
+                        if len(param_found) == 0: continue
+                        else:
+                            # Get the value of the string 
+                            # (the first element after the string without [spaces, new lines, =])
+                            start = line.find(other_time_string)
+                            end = start + len(other_time_string)
+                            res = line[end:].strip('%=\n\r ')
+                            res_split = res.split(" ")
+                            csv_file.write(res_split[0])
+                            if string_idx < len(find_string)-1:
+                                csv_file.write(",")
+                            found = True
+                            break
+                if not found:
+                    csv_file.write("-1,")
     csv_file.write("\n")
 
 csv_file.close()
