7685c58d5a422e69f07cedf4a7fceed8f5c6efce
marvin.porsil <marvin.porsil@rwth-aachen.de>
2021-06-18 12:24:35 +0200

    Batchjob script now working correct. Fixed OMP only using core 0. Changed the directory structure which will be automatically created for each job.

--- affinity_tests_mxm/create_batchjobs.sh
+++ affinity_tests_mxm/create_batchjobs.sh
@@ -1,12 +1,17 @@
 #!/usr/local_rwth/bin/zsh
 
 export CUR_DATE_STR=${CUR_DATE_STR:-"$(date +"%Y%m%d_%H%M%S")"}
-export OUT_DIR="/home/ka387454/repos/chameleon-scripts/affinity_tests_mxm/outputs/Output_"${CUR_DATE_STR}
+export OUT_DIR="/home/ka387454/repos/chameleon-scripts/affinity_tests_mxm/outputs/output_"${CUR_DATE_STR}
 mkdir ${OUT_DIR}
 
-export_vars="OUT_DIR"
+export_vars="OUT_DIR,CUR_DATE_STR"
 
-sbatch --nodes=4 --ntasks-per-node=1 --job-name=mxm_affinity_testing \
---output=./outputs/mxm_test_${CUR_DATE_STR}.%J.txt \
+# sbatch --nodes=1 --ntasks-per-node=1 --cpus-per-task=48 --job-name=mxm_affinity_testing \
+# --output=${OUT_DIR}/slurmOutput.txt \
+# --export=${export_vars} \
+# run_experiments.sh
+
+sbatch --nodes=4 --ntasks-per-node=1 --cpus-per-task=48 --job-name=mxm_affinity_testing \
+--output=${OUT_DIR}/slurmOutput.txt \
 --export=${export_vars} \
 run_experiments.sh
--- affinity_tests_mxm/run_experiments.sh
+++ affinity_tests_mxm/run_experiments.sh
@@ -1,5 +1,5 @@
 #!/usr/local_rwth/bin/zsh
-#SBATCH --time=00:10:00
+#SBATCH --time=00:15:00
 #SBATCH --exclusive
 #SBATCH --partition=c18m
 
@@ -21,9 +21,14 @@ export PROG="mxm_chameleon"
 export CHAMELEON_VERSION="chameleon/intel"
 export MXM_PARAMS="600 1200 1200 1200 1200"
 
+N_RUNS=5
+
 cd /home/ka387454/repos/chameleon-apps/applications/matrix_example
 module use -a /home/ka387454/.modules
 module load $CHAMELEON_VERSION
+
+LOG_DIR=${OUT_DIR}"/logs"
+mkdir ${LOG_DIR}
 
 export_vars="CHAMELEON_VERSION,MXM_PARAMS,OMP_NUM_THREADS,CHAM_AFF_TASK_SELECTION_STRAT,CHAM_AFF_PAGE_SELECTION_STRAT,CHAM_AFF_PAGE_WEIGHTING_STRAT,CHAM_AFF_CONSIDER_TYPES,CHAM_AFF_PAGE_SELECTION_N,CHAM_AFF_TASK_SELECTION_N,CHAM_AFF_MAP_MODE,CHAM_AFF_ALWAYS_CHECK_PHYSICAL,NO_NUMA_BALANCING,AUTOMATIC_NUMA_BALANCING,PROG"
 
@@ -53,6 +58,7 @@ PERCENTAGE_DIFF_TASKS_TO_MIGRATE,ENABLE_TRACE_FROM_SYNC_CYCLE,ENABLE_TRACE_TO_SY
 ${export_vars} \
 ${NO_NUMA_BALANCING} \
 ./${PROG} ${MXM_PARAMS}
+
 }
 
 #########################################################
@@ -61,14 +67,17 @@ ${NO_NUMA_BALANCING} \
 for VAR1 in 0 1 2 3
 do
     export CHAM_AFF_MAP_MODE=${VAR1}
-    for VAR2 in {0..8}
+    for VAR2 in {0..4}
     do
     export CHAM_AFF_TASK_SELECTION_STRAT=${VAR2}
-    VARIATION_DIR=${OUT_DIR}"/"${VAR1}"_"${VAR2}
+    VARIATION_DIR=${LOG_DIR}"/"${VAR1}"_"${VAR2}
     mkdir ${VARIATION_DIR}
-        for RUN in {1..20}
+        for RUN in {1..${N_RUNS}}
         do
-            eval "run_experiment" >> ${VARIATION_DIR}/R${RUN}.log
+            eval "run_experiment" >>& ${VARIATION_DIR}/R${RUN}.log
+            # print some information to check the progression of the job
+            squeue -u ka387454 >> ${OUT_DIR}/runtime_progression.log
+            echo "Finished"${VAR1}"_"${VAR2}"_R"${RUN} >> ${OUT_DIR}/runtime_progression.log
         done
     done
 done
@@ -76,20 +85,58 @@ done
 ##############################
 # Chameleon without affinity #
 ##############################
-VARIATION_DIR=${OUT_DIR}"/cham_no_affinity"
-for RUN in {1..20}
+export CHAMELEON_VERSION="chameleon/intel_no_affinity"
+module load $CHAMELEON_VERSION
+VARIATION_DIR=${LOG_DIR}"/cham_no_affinity"
+mkdir ${VARIATION_DIR}
+for RUN in {1..${N_RUNS}}
 do
-    eval "run_experiment" >> ${VARIATION_DIR}/R${RUN}.log
+    eval "run_experiment" >>& ${VARIATION_DIR}/R${RUN}.log
+    # print some information to check the progression of the job
+    squeue -u ka387454 >> ${OUT_DIR}/runtime_progression.log
+    echo "Finished_no_affinity_R"${RUN} >> ${OUT_DIR}/runtime_progression.log
 done
 
 ##############################
 # Tasking only, no chameleon #
 ##############################
-VARIATION_DIR=${OUT_DIR}"/no_chameleon"
 export CHAMELEON_VERSION="chameleon/intel_no_affinity" # Need to load chameleon because of prints in the matrix example
+module load $CHAMELEON_VERSION
+VARIATION_DIR=${LOG_DIR}"/no_chameleon"
+mkdir ${VARIATION_DIR}
 export PROG=mxm_tasking
 export OMP_NUM_THREADS=48
-for RUN in {1..20}
+for RUN in {1..${N_RUNS}}
 do
-    eval "run_experiment" >> ${VARIATION_DIR}/R${RUN}.log
+    eval "run_experiment" >>& ${VARIATION_DIR}/R${RUN}.log
+    # print some information to check the progression of the job
+    squeue -u ka387454 >> ${OUT_DIR}/runtime_progression.log
+    echo "Finished_no_chameleon_R"${RUN} >> ${OUT_DIR}/runtime_progression.log
 done
+
+########################################
+# Single independent job for debugging #
+########################################
+# export MXM_PARAMS="600 1200"
+# export CHAMELEON_VERSION="chameleon/intel"
+# module load $CHAMELEON_VERSION
+# export CHAM_AFF_TASK_SELECTION_STRAT=4
+# export CHAM_AFF_PAGE_SELECTION_STRAT=8
+# export CHAM_AFF_PAGE_WEIGHTING_STRAT=2
+# export CHAM_AFF_CONSIDER_TYPES=1
+# export CHAM_AFF_PAGE_SELECTION_N=3
+# export CHAM_AFF_TASK_SELECTION_N=3
+# export CHAM_AFF_MAP_MODE=0
+# export CHAM_AFF_ALWAYS_CHECK_PHYSICAL=1
+# export AUTOMATIC_NUMA_BALANCING=1
+# export OMP_NUM_THREADS=47
+# export PROG="mxm_chameleon"
+# VARIATION_DIR=${LOG_DIR}"/single_test"
+# mkdir ${VARIATION_DIR}
+# for RUN in 1
+# do
+#     eval "run_experiment" >>& ${VARIATION_DIR}/R${RUN}.log
+# done
+
+# get the total runtime of the job
+squeue -u ka387454
